845837da38035d43f9da7d6b322e27aca51e8b55
Ram5anthosh <ramsanthosh2112@gmail.com>
2023-08-04 12:41:26 +0530

    Added basic functionality

--- src/App.js
+++ src/App.js
@@ -1,9 +1,56 @@
 import logo from './logo.svg';
 import './App.css';
 
+import Header from "./components/Header";
+import Tasks from "./components/Tasks";
+import TaskEdit from "./components/TaskEdit";
+
+import {useState} from "react";
+
 function App() {
+  const [tasks,setTasks]=useState([
+    {desc : "Sample Task 1", id:1, date:"02-03-2023 09:00", status: "Complete"},
+    {desc : "Sample Task 2", id:2, date:"04-07-2023 10:00", status: "Open"},
+  ]);
+
+  const onTglStatus = (task) => {
+    console.log("completing task");
+    setTasks(
+      tasks.map((chkTask) => {
+        chkTask.complete =
+          task.id === chkTask.id ? !chkTask.complete : chkTask.complete;
+        return chkTask;
+      })
+    );
+  };
+
+  const [showTaskEdit, setShowTaskEdit] = useState(false);
+  
+  const onSaveTask = ({ desc, date }) => {
+    console.log("saving tasks");
+    setTasks([
+      { desc: desc, date: date, id: Date.now(), complete: false },
+      ...tasks,
+    ]);
+  };  
+
+
   return (
     <div className="App">
+      <Header></Header>
+      <button
+  className="button outline"
+  onClick={() => setShowTaskEdit(!showTaskEdit)}>
+  {!showTaskEdit && "New"}
+  {showTaskEdit && "➖"}
+</button>;
+  {
+  showTaskEdit && <TaskEdit task={{}} onSaveTask={onSaveTask} />
+  }
+      <div className="container">
+        <Tasks tasks={tasks} onTglStatus={onTglStatus}></Tasks>
+      </div>
+      {/* 
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p className="font-bold">
@@ -18,6 +65,7 @@ function App() {
           Learn React
         </a>
       </header>
+  */}
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,21 @@
+function Header() {
+    return (
+      <div>
+        <nav className="nav rounded-lg px-3 py-2 text-slate-700 font-medium hover:bg-slate-100 hover:text-slate-900">
+          <div className="nav-left">
+            <a className="brand" href="#">
+              Tasker
+            </a>
+          </div>
+          <div className="nav-right">
+            <div className="tabs">
+              <a href="https://www.linkedin.com/in/ram-santhosh-patnaik-belagam-b96729253/">Ram5anthosh</a>
+            </div>
+          </div>
+        </nav>
+      </div>
+    );
+  }
+  
+  export default Header;
+  
